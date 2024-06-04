0c9905c2cd42fc76e1848b5faf8d1bf7041f0e8c
AnnaKyuenkova <113756994+AnnaKyuenkova@users.noreply.github.com>
2024-06-05 01:43:00 +0300

    lab4 update

--- lab4/app/app.py
+++ lab4/app/app.py
@@ -24,26 +24,38 @@ class User(UserMixin):
         self.id = user_id
         self.login = user_login
 
-def pas_check_len(password):
-    if len(password) < 8 or len(password) > 128:
-        return False
-    else:
-        return True
-    
-def pas_check_char(password):
-    lowalpha = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-    upalpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-    lowcount, upcount = 0, 0
+##### ДОМАШНЕЕ ЗАДАНИЕ #####
+
+# ВАЛИДАЦИЯ 
+
+# проверка на длину пароля (вернет false при неправильной длине)
+def password_check_len(password):
+    return len(password) > 8 or len(password) < 128
+
+# проверка на одну заглавную и одну строчную (вернет false при отсутсвии строчной или заглавной буквы)
+def password_check_register(password):
+    has_lowercase = False
+    has_uppercase = False
+    for char in password:
+        if char.islower():
+            has_lowercase = True
+        elif char.isupper():
+            has_uppercase = True
+    return has_lowercase and has_uppercase
+
+# проверка алфавита (вернет false, если не будет букв)
+def password_check_char(password):
+    alpha = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    count = 0
     for i in password:
-        if i in lowalpha:
-            lowcount += 1
-        elif i in upalpha:
-            upcount += 1
-    if lowcount == 0 or upcount == 0:
+        if i in alpha:
+            count += 1
+    if count == 0:
         return False
     return True
 
-def pas_check_num(password):
+# проверка на цифры в пароле (вернет false при отсутствии цифр)
+def password_check_num(password):
     nums = '0123456789'
     numcount = 0
     for i in password:
@@ -53,7 +65,8 @@ def pas_check_num(password):
         return False
     return True
     
-def pas_check_symbols(password):
+# проверка на допустимые символы
+def password_check_symbols(password):
     lowalpha = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     upalpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
     nums = '0123456789'
@@ -68,59 +81,61 @@ def pas_check_symbols(password):
             return False
     return True
 
-def pas_validation(password):
-    if not pas_check_len(password):
+# проверка условий для пароля
+def password_validation(password):
+    if not password_check_len(password):
         return False
-    if not pas_check_char(password):
+    if not password_check_register(password):
         return False
-    if not pas_check_num(password):
+    if not password_check_char(password):
         return False
-    if not pas_check_symbols(password):
+    if not password_check_num(password):
+        return False
+    if not password_check_symbols(password):
         return False
     return True
 
-def log_validation(login):
+# проверка длины логина (вернет false при недопустимой длине)
+def login_check_len(login):
+    if len(login) > 5:
+        return True
+    return False
+
+# допустимые символы логина (вернет false при недопустимых символах)
+def login_validation(login):
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     for char in login:
         if char not in alphabet:
             return False
-    if len(login) < 5:
-        return False
     return True
 
 def validate(login, password, last_name, first_name):
     errors = {}
-    if not pas_validation(password):
-        pas_mes = '''
-Пароль не соответствует требованиям:
-'''
-        if not pas_check_len(password):
-            pas_mes += '''
-не менее 8 символов;
-не более 128 символов;
-'''
-        if not pas_check_char(password):
-            pas_mes += '''
-как минимум одна заглавная и одна строчная буква;
-только латинские или кириллические буквы;
-'''
-        if not pas_check_num(password):
-           pas_mes +=  '''
-как минимум одна цифра;
-только арабские цифры;
-'''
-        if not pas_check_symbols(password):
-            pas_mes += '''без пробелов;
-Другие допустимые символы:~ ! ? @ # $ % ^ & * _ - + ( ) [ ] { } > < / \ | " ' . , : ;
-'''
+    if not password_validation(password):
+        password_mes = 'Пароль не соответствует требованиям: '
+        if password_check_len(password):
+            password_mes += 'не менее 8 символов и не более 128 символов; '
+        if not password_check_register(password):
+            password_mes += 'как минимум одна заглавная и одна строчная буква; '
+        if not password_check_char(password):
+            password_mes += 'только латинские или кириллические буквы; '
+        if not password_check_num(password):
+           password_mes +=  'как минимум одна цифра; только арабские цифры; '
+        if not password_check_symbols(password):
+            password_mes += '''без пробелов; Другие допустимые символы:~ ! ? @ # $ % ^ & * _ - + ( ) [ ] { } > < / \ | " ' . , : ;'''
 
         errors['pas_class'] = "is-invalid"
         errors['pas_mes_class'] = 'invalid-feedback'
-        errors['pas_mes'] = pas_mes
-        if not log_validation(login):
+        errors['pas_mes'] = password_mes
+
+        if not login_validation(login):
             errors['log_class'] = 'is-invalid'
             errors['log_mes_class'] = 'invalid-feedback'
-            errors['log_mes'] = 'Логин должен состоять только из латинских букв и цифр и иметь длину не менее 5 символов'
+            errors['log_mes'] = 'Логин должен состоять только из латинских букв и цифр'
+        if not login_check_len(login):
+            errors['log_class'] = 'is-invalid'
+            errors['log_mes_class'] = 'invalid-feedback'
+            errors['log_mes'] = 'Логин должен быть не менее 5 символов'
         if len(login) == 0:
             errors['log_class'] = "is-invalid"
             errors['log_mes_class'] = "invalid-feedback"
@@ -138,6 +153,54 @@ def validate(login, password, last_name, first_name):
             errors['fn_mes_class'] = "invalid-feedback"
             errors['fn_mes'] = "Поле имя не должно быть пустым" 
         return errors
+
+
+# ИЗМЕНЕНИЕ ПАРОЛЯ
+@app.route('/users/change/', methods=["POST", "GET"])
+@login_required
+def change():
+    if request.method == "POST":
+        user_id = current_user.id
+        oldpas = request.form['oldpas']
+        newpas = request.form['newpas']
+        new2pas = request.form['new2pas']
+        query = 'SELECT * FROM users WHERE id = %s and password_hash = %s'
+        user = None
+        errors = validate('login', newpas, 'name', 'name')
+        try:
+            cursor = db.connection().cursor(named_tuple=True)
+            cursor.execute(query, (user_id, oldpas))
+            user = cursor.fetchone()
+            cursor.close()
+        except mysql.connector.errors.DatabaseError:
+            db.connection().rollback()
+            flash(f'Возникла ошибка при проверке пароля', 'danger')
+            return render_template('users/change.html')
+        if not user:
+            flash(f'Неверный пароль', 'danger')
+            return render_template('users/change.html')
+        elif errors:
+            if len(errors.keys()) > 0:
+                flash(errors['pas_mes'], 'danger')
+                return render_template('users/change.html')
+        elif newpas != new2pas:
+            flash(f'Пароли не совпадают', 'danger')
+            return render_template('users/change.html')
+        else:
+            query = 'UPDATE users SET password_hash = %s WHERE id = %s'
+            try:
+                cursor = db.connection().cursor(named_tuple=True)
+                cursor.execute(query, (newpas, user_id))
+                db.connection().commit()
+                cursor.close()
+                flash(f'Пароль обновлен', 'success')
+                return redirect(url_for('index'))
+            except mysql.connector.errors.DatabaseError:
+                db.connection().rollback()
+                flash(f'Ошибка при обновлении пароля', 'danger')
+                return render_template('users/change.html')
+    else:
+        return render_template('users/change.html')
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -181,48 +244,29 @@ def logout():
 @app.route('/users/')
 @login_required
 def show_users():
-    query = 'SELECT * FROM users'
+    query = '''
+            SELECT users.*, roles.name as role_name
+            FROM users
+            LEFT JOIN roles
+            on roles.id = users.role_id
+            '''
     cursor = db.connection().cursor(named_tuple=True)
     cursor.execute(query)
     users = cursor.fetchall()
     cursor.close()
     return render_template('users/index.html',users=users)
 
-@app.route('/users/create', methods = ['POST', 'GET'])
-@login_required
-def create():
-    if request.method == 'POST':
-        login = request.form['login']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        middle_name = request.form['middle_name']
-        password = request.form['password']
-
-        errors = validate(login, password, last_name, first_name)
-        if errors:
-            if len(errors.keys()) > 0:
-                return render_template('users/create.html', **errors)
-
-        try:
-            query = '''
-                insert into users (login, last_name, first_name, middle_name, password_hash)
-                VALUES (%s, %s, %s, %s, %s)
-                '''
-            cursor = db.connection().cursor(named_tuple=True)
-            cursor.execute(query, (login, last_name, first_name, middle_name, password))
-            db.connection().commit()
-            flash(f'Пользователь {login} успешно создан.', 'success')
-            cursor.close()
-        except mysql.connector.errors.DatabaseError:
-            db.connection().rollback()
-            flash(f'При создании пользователя произошла ошибка.', 'danger')
-            return render_template('users/create.html')
-        
-    return render_template('users/create.html')
+def get_roles():
+    query = 'SELECT * FROM roles'
+    cursor = db.connection().cursor(named_tuple=True)
+    cursor.execute(query)
+    roles = cursor.fetchall()
+    cursor.close()
+    return roles
 
 @app.route('/users/show/<int:user_id>') 
 def show_user(user_id):
-    user_query = 'SELECT users.*, roles.name AS role_name FROM users JOIN roles ON users.role_id = roles.id WHERE users.id=%s'
+    user_query = 'SELECT users.*, roles.name AS role_name FROM users LEFT JOIN roles ON users.role_id = roles.id WHERE users.id=%s'
     
     with db.connection().cursor(named_tuple=True) as cursor:
         cursor.execute(user_query, (user_id,))
@@ -230,105 +274,91 @@ def show_user(user_id):
         
     return render_template('users/show.html', user=user)
 
+@app.route('/users/create', methods = ['POST', 'GET'])
+@login_required
+def create():
+    roles = get_roles()
+    if request.method == 'POST':
+        login = request.form['login']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        middle_name = request.form['middle_name']
+        password = request.form['password']
+        role_id = request.form['role_id']
+
+        errors = validate(login, password, last_name, first_name)
+        if errors:
+            if len(errors.keys()) > 0:
+                return render_template('users/create.html', **errors)
+
+        try:
+            query = 'INSERT INTO users (login, last_name, first_name, middle_name, password_hash, role_id) VALUES (%s, %s, %s, %s, %s, %s)'
+            cursor = db.connection().cursor(named_tuple=True)
+            cursor.execute(query, (login, last_name, first_name, middle_name, password, role_id))
+            db.connection().commit()
+            flash(f'Пользователь {login} успешно создан.', 'success')
+            cursor.close()
+            return redirect(url_for('show_users'))
+        
+        except mysql.connector.errors.DatabaseError:
+            db.connection().rollback()
+            flash(f'При создании пользователя произошла ошибка.', 'danger')
+            return render_template('users/create.html', roles=roles)
+        
+    return render_template('users/create.html')
+
 @app.route('/users/edit/<int:user_id>', methods=["POST", "GET"])
 def edit(user_id):
+    roles = get_roles()
     if request.method == 'POST':
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         middle_name = request.form['middle_name']
+        role_id = request.form['role_id']
         try:
-            query = '''
-                UPDATE users set first_name = %s, last_name = %s, middle_name = %s where id = %s
-                '''
+            query = 'UPDATE users set first_name = %s, last_name = %s, middle_name = %s, role_id = %s where id = %s'
             cursor = db.connection().cursor(named_tuple=True)
-            cursor.execute(query, (first_name, last_name, middle_name, user_id))
+            cursor.execute(query, (first_name, last_name, middle_name, role_id, user_id))
             db.connection().commit()
             flash(f'Данные пользователя {first_name} успешно обновлены.', 'success')
             cursor.close()
+            return redirect(url_for('show_users'))
+        
         except mysql.connector.errors.DatabaseError:
             db.connection().rollback()
             flash(f'При обновлении пользователя произошла ошибка.', 'danger')
             return render_template('users/edit.html')
 
-    query = 'SELECT  * FROM users WHERE users.id=%s'
+    query = '''
+            SELECT users.*, roles.name as role_name
+            FROM users
+            LEFT JOIN roles
+            on roles.id = users.role_id
+            where users.id=%s
+            '''
     cursor = db.connection().cursor(named_tuple=True)
     cursor.execute(query, (user_id,))
     user = cursor.fetchone()
     cursor.close()
 
-    return render_template('users/edit.html', user=user)
+    return render_template('users/edit.html', user=user, roles=roles)
 
 @app.route('/users/delete/')
 @login_required
 def delete():
+
     try:
         user_id = request.args.get('user_id')
-        query = '''
-            DELETE from users where id = %s
-            '''
+        query = 'DELETE from users where id = %s'
         cursor = db.connection().cursor(named_tuple=True)
         cursor.execute(query, (user_id,))
         db.connection().commit()
-        flash(f'Пользователь {user_id} успешно удален.', 'success')
+        flash(f'Этот пользователь успешно удален.', 'success')
         cursor.close()
+
     except mysql.connector.errors.DatabaseError:
         db.connection().rollback()
         flash(f'При удалении пользователя произошла ошибка.', 'danger')
         return render_template('users/index.html', user_id=user_id)
 
     return redirect(url_for('show_users'))
-
-@app.route('/users/change/', methods=["POST", "GET"])
-@login_required
-def change():
-    if request.method == "POST":
-        user_id = current_user.id
-        oldpas = request.form['oldpas']
-        newpas = request.form['newpas']
-        new2pas = request.form['new2pas']
-        query = """
-            SELECT * FROM users WHERE id = %s and password_hash = SHA2(%s, 256)
-            """
-        user = None
-        errors = validate('login', newpas, 'name', 'name')
-        try:
-            cursor = db.connection().cursor(named_tuple=True)
-            cursor.execute(query, (user_id, oldpas))
-            user = cursor.fetchone()
-            cursor.close()
-        except mysql.connector.errors.DatabaseError:
-            db.connection().rollback()
-            flash(f'Возникла ошибка при проверке пароля', 'danger')
-            return render_template('users/change.html')
-        if not user:
-            flash(f'Неправильный пароль', 'danger')
-            return render_template('users/change.html')
-        # elif not pas_validation(newpas):
-        elif errors:
-            if len(errors.keys()) > 0:
-                flash(errors['pas_mes'], 'danger')
-                return render_template('users/change.html')
-        elif newpas != new2pas:
-            flash(f'Пароли не совпадают', 'danger')
-            return render_template('users/change.html')
-        else:
-            query = """
-                UPDATE users SET password_hash = SHA2(%s, 256) WHERE id = %s
-                """
-            try:
-                cursor = db.connection().cursor(named_tuple=True)
-                cursor.execute(query, (newpas, user_id))
-                db.connection().commit()
-                cursor.close()
-
-                # with db.connection().cursor(named_tuple=True) as cursor:
-                #     cursor.execute(query, (newpas, user_id))
-                #     db.connection().commit()
-                flash(f'Пароль обновлен', 'success')
-                return redirect(url_for('index'))
-            except mysql.connector.errors.DatabaseError:
-                db.connection().rollback()
-                flash(f'Ошибка при обновлении пароля', 'danger')
-                return render_template('users/change.html')
-    else:
-        return render_template('users/change.html')
